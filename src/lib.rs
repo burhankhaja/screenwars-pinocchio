72a1eaeabef25efae8b5000710e5f5f69451f4ab
burhankhaja <karhazemail@gmail.com>
2025-09-04 08:58:15 +0000

    handle initialize entrypoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,28 @@
 #![no_std]
 
-use pinocchio::{nostd_panic_handler, entrypoint, account_info::AccountInfo, ProgramResult};
+use pinocchio::{
+    account_info::AccountInfo, entrypoint, nostd_panic_handler, program_error::ProgramError,
+    ProgramResult,
+};
+
+pub mod instructions;
+pub mod state;
+pub use instructions::*;
+pub use state::*;
 
 nostd_panic_handler!();
 entrypoint!(process_instruction);
 
 pinocchio_pubkey::declare_id!("ScreenWars111111111111111111111111111111111");
 
-
 pub fn process_instruction(
-    _program_id : &[u8; 32],
-    _accounts : &[AccountInfo],
-    _instruction_data : &[u8],
+    _program_id: &[u8; 32],
+    _accounts: &[AccountInfo],
+    _instruction_data: &[u8],
 ) -> ProgramResult {
+    match _instruction_data.split_first() {
+        Some((Initialize::DISCRIMINATOR, _)) => Initialize::try_from(_accounts)?.process()?,
+        _ => Err(ProgramError::InvalidInstructionData)?,
+    }
     Ok(())
 }
