9e1c8be67a950ff5e2baf8a787a0c47fe010c884
burhankhaja <karhazemail@gmail.com>
2025-09-09 14:00:14 +0000

    handle create_challenge entrypoint and fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,18 @@
 #![no_std]
 
-use pinocchio::{
-    account_info::AccountInfo, entrypoint, nostd_panic_handler, program_error::ProgramError,
-    ProgramResult,
-};
-
 pub mod instructions;
 pub mod state;
-pub use instructions::*;
-pub use state::*;
+pub mod custom_errors;
+
+pub use {
+    custom_errors::ScreenWarErrors,
+    instructions::*,
+    pinocchio::{
+        account_info::AccountInfo, entrypoint, nostd_panic_handler, program_error::ProgramError,
+        ProgramResult,
+    },
+    state::*,
+};
 
 nostd_panic_handler!();
 entrypoint!(process_instruction);
@@ -22,6 +26,9 @@ pub fn process_instruction(
 ) -> ProgramResult {
     match _instruction_data.split_first() {
         Some((Initialize::DISCRIMINATOR, _)) => Initialize::try_from(_accounts)?.process()?,
+        Some((CreateChallenge::DISCRIMINATOR, data)) => {
+            CreateChallenge::try_from((_accounts, data))?.process()?
+        }
         _ => Err(ProgramError::InvalidInstructionData)?,
     }
     Ok(())
