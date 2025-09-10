280320a26de85ae4729485e42116e747eeb96ce2
burhankhaja <karhazemail@gmail.com>
2025-09-10 07:37:52 +0000

    add impl to User state

--- src/state/user.rs
+++ src/state/user.rs
@@ -1,4 +1,8 @@
-use pinocchio::{pubkey::Pubkey};
+use {
+    crate::custom_errors::ScreenWarErrors,
+    core::mem::size_of,
+    pinocchio::{program_error::ProgramError, pubkey::Pubkey},
+};
 
 #[repr(C)]
 #[derive(Default, Debug)]
@@ -8,4 +12,34 @@ pub struct User {
     pub locked_balance: u64,
     pub streak: u8,
     pub bump: u8,
+}
+
+impl User {
+    pub const LEN: usize = size_of::<Pubkey>()
+        + size_of::<u32>()
+        + size_of::<u64>()
+        + size_of::<u8>()
+        + size_of::<u8>();
+
+    pub fn load_mut(bytes: &mut [u8]) -> Result<&mut Self, ProgramError> {
+        if bytes.len().ne(&Self::LEN) {
+            return Err(ScreenWarErrors::InvalidPdaDataLen.into());
+        }
+        let ptr = bytes.as_mut_ptr() as *mut Self;
+
+        let user_pda = unsafe { &mut *ptr };
+
+        Ok(user_pda)
+    }
+
+    pub fn load(bytes: &[u8]) -> Result<&Self, ProgramError> {
+        if bytes.len().ne(&Self::LEN) {
+            return Err(ScreenWarErrors::InvalidPdaDataLen.into());
+        }
+        let ptr = bytes.as_ptr() as *const Self;
+
+        let user_pda = unsafe { &*ptr };
+
+        Ok(user_pda)
+    }
 }
