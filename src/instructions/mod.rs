1286f68471d0a843ec5b719ae6663e1440286e98
burhankhaja <karhazemail@gmail.com>
2025-09-10 13:23:36 +0000

    udpate withdraw in instruction mod

--- src/instructions/mod.rs
+++ src/instructions/mod.rs
@@ -1,5 +1,6 @@
 pub mod create_challenge;
 pub mod initialize;
 pub mod join_challenge;
+pub mod withdraw;
 
-pub use {create_challenge::*, initialize::*, join_challenge::*};
+pub use {create_challenge::*, initialize::*, join_challenge::*, withdraw::*};
