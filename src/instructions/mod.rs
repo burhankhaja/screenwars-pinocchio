16108828f405bf01d4234dcb65f7d9c9cea63610
burhankhaja <karhazemail@gmail.com>
2025-09-10 07:36:46 +0000

    update instruction mod

--- src/instructions/mod.rs
+++ src/instructions/mod.rs
@@ -1,4 +1,5 @@
 pub mod create_challenge;
 pub mod initialize;
+pub mod join_challenge;
 
-pub use {create_challenge::*, initialize::*};
+pub use {create_challenge::*, initialize::*, join_challenge::*};
