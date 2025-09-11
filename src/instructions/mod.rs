2e52b44984e606971c7cf2a95e8ebc0dbe1903d5
burhankhaja <karhazemail@gmail.com>
2025-09-11 17:08:20 +0000

    update instruction mod to use all new instructions

--- src/instructions/mod.rs
+++ src/instructions/mod.rs
@@ -1,6 +1,14 @@
+pub mod claim_winner_position;
 pub mod create_challenge;
 pub mod initialize;
 pub mod join_challenge;
+pub mod rewards;
+pub mod sync_lock;
+pub mod take_profit;
+pub mod toggle_challenge_creation;
 pub mod withdraw;
 
-pub use {create_challenge::*, initialize::*, join_challenge::*, withdraw::*};
+pub use {
+    claim_winner_position::*, create_challenge::*, initialize::*, join_challenge::*, rewards::*,
+    sync_lock::*, take_profit::*, toggle_challenge_creation::*, withdraw::*,
+};
