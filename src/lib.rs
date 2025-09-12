b2410ab5c59688cffbc6b5611e3ddbe9f9e610a9
burhankhaja <karhazemail@gmail.com>
2025-09-12 06:58:05 +0000

    add entrypoint to all instructions in lib

--- src/lib.rs
+++ src/lib.rs
@@ -23,16 +23,52 @@ pinocchio_pubkey::declare_id!("ScreenWars111111111111111111111111111111111");
 
 pub fn process_instruction(
     _program_id: &[u8; 32],
-    _accounts: &[AccountInfo],
-    _instruction_data: &[u8],
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
 ) -> ProgramResult {
-    match _instruction_data.split_first() {
-        Some((Initialize::DISCRIMINATOR, _)) => Initialize::try_from(_accounts)?.process()?,
+    match instruction_data.split_first() {
+        // initialize
+        Some((Initialize::DISCRIMINATOR, _)) => Initialize::try_from(accounts)?.process()?,
+
+        // create challenge
         Some((CreateChallenge::DISCRIMINATOR, data)) => {
-            CreateChallenge::try_from((_accounts, data))?.process()?
+            CreateChallenge::try_from((accounts, data))?.process()?
         }
+
+        // join challenge
         Some((JoinChallenge::DISCRIMINATOR, data)) => {
-            JoinChallenge::try_from((_accounts, data))?.process()?
+            JoinChallenge::try_from((accounts, data))?.process()?
+        }
+
+        // sync and lock
+        Some((SyncLock::DISCRIMINATOR, data)) => SyncLock::try_from((accounts, data))?.process()?,
+
+        // claim winner position
+        Some((ClaimWinnerPosition::DISCRIMINATOR, data)) => {
+            ClaimWinnerPosition::try_from((accounts, data))?.process()?
+        }
+
+        // withdraw locked funds
+        Some((Withdraw::DISCRIMINATOR, data)) => Withdraw::try_from((accounts, data))?.process()?,
+
+        // claim rewards as winner
+        Some((ClaimRewards::WINNER_REWARD_DISCRIMINATOR, data)) => {
+            ClaimRewards::try_from((accounts, data))?.process_winner_rewards()?
+        }
+
+        // claim rewards as creator
+        Some((ClaimRewards::CREATOR_REWARD_DISCRIMINATOR, data)) => {
+            ClaimRewards::try_from((accounts, data))?.process_creator_rewards()?
+        }
+
+        // take protocol profits (#admin)
+        Some((TakeProfit::DISCRIMINATOR, data)) => {
+            TakeProfit::try_from((accounts, data))?.process()?
+        }
+
+        // pause unpause challenge creation (#admin)
+        Some((ToggleChallengeCreation::DISCRIMINATOR, data)) => {
+            ToggleChallengeCreation::try_from((accounts, data))?.process()?
         }
 
         _ => Err(ProgramError::InvalidInstructionData)?,
